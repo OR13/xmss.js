dbc7243ed28af39fcd996e26cdd19d105047d9e4
Orie Steele <orie@transmute.industries>
2022-04-02 16:21:14 -0500

    chore: fix interface

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,33 @@
 import { init } from './main';
 
-export const sum = async (a: number, b: number) => {
+export const generate = async () => {
   const wasm = await init();
-  return wasm.add(a, b);
+  const request = JSON.stringify({ command: 'generate' });
+  const response = wasm.handleRequest(request);
+  return JSON.parse(response);
+};
+
+export const sign = async (message: string, jwk: any) => {
+  const wasm = await init();
+  const request = JSON.stringify({
+    command: 'sign',
+    message,
+    jwk: JSON.stringify(jwk),
+  });
+  const response = wasm.handleRequest(request);
+  const parsed = JSON.parse(response);
+  return parsed.signature;
+};
+
+export const verify = async (message: string, signature: string, jwk: any) => {
+  const wasm = await init();
+  const request = JSON.stringify({
+    command: 'verify',
+    message,
+    signature,
+    jwk: JSON.stringify(jwk),
+  });
+  const response = wasm.handleRequest(request);
+  const parsed = JSON.parse(response);
+  return parsed.verified;
 };
--- src/main.go
+++ src/main.go
@@ -2,16 +2,69 @@ package main
 
 import (
 	"syscall/js"
+	"encoding/json"
+	"encoding/base64"
+	"example.com/hello/src/keypair"
 )
 
-func add(this js.Value, args []js.Value) interface{} {
-	return js.ValueOf(args[0].Int() + args[1].Int())
+
+func Base64UrlEncode(src []byte) []byte {
+    return []byte(base64.RawURLEncoding.EncodeToString(src))
+}
+
+func Base64UrlDecode(src []byte) ([]byte, error) {
+    return base64.RawURLEncoding.DecodeString(string(src))
+}
+
+func handleRequest(this js.Value, args []js.Value) interface{} {
+	
+	requestString := args[0].String()
+	requestMap := map[string]string{}
+    json.Unmarshal([]byte(requestString), &requestMap)
+
+	if (requestMap["command"] == "generate"){
+		res, err := keypair.Generate()
+		if err != nil {
+			return js.ValueOf(nil)
+		}
+		return js.ValueOf(res)
+	}
+
+	if (requestMap["command"] == "sign"){
+		message, err := Base64UrlDecode([]byte(requestMap["message"]))
+		if err != nil {
+			return js.ValueOf(nil)
+		}
+		publicKeyJwk := requestMap["jwk"]
+		res, err := keypair.Sign(message, publicKeyJwk)
+		if err != nil {
+			return js.ValueOf(nil)
+		}
+		return js.ValueOf(res)
+	}
+
+	if (requestMap["command"] == "verify"){
+		message, err := Base64UrlDecode([]byte(requestMap["message"]))
+		if err != nil {
+			return js.ValueOf(nil)
+		}
+		signature, err := Base64UrlDecode([]byte(requestMap["signature"]))
+		if err != nil {
+			return js.ValueOf(nil)
+		}
+		publicKeyJwk := requestMap["jwk"]
+		res, err := keypair.Verify(message, signature, publicKeyJwk)
+		if err != nil {
+			return js.ValueOf(nil)
+		}
+		return js.ValueOf(res)
+	}
+
+	return js.ValueOf(nil)
 }
 
 func init() {
-	// we have to declare our functions in an init func otherwise they aren't
-	// available in JS land at the call time.
-	js.Global().Set("add", js.FuncOf(add))
+	js.Global().Set("handleRequest", js.FuncOf(handleRequest))
 }
 
 func main() {
